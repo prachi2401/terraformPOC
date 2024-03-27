6e418157beb9684a09386a7c7c423bf36eb71ab4
Prachi Mittal <prachi.mittal@celebaltech.com>
2024-03-27 15:24:06 +0530

    Test

--- Main.tf
+++ Main.tf
@@ -1,5 +1,20 @@
-provider "azurerm"{
-  features {}
- 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+  backend "azurerm" {
+      resource_group_name  = "Gaurav_RG"
+      storage_account_name = "storage132"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+      access_key           = "mykNZ6ip6Lf/xQfzbri13JRVCXd3zLLCpC5LrKrcbY1jeLOtSgjEAPeYjy8hvcIPZnYN3WXDuWLH+ASt1MEehw=="
+  }
+
 }
 
+provider "azurerm" {
+  features {}
+}
--- Vnet.tf
+++ Vnet.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "Vnet" {
   name                =  var.vnet_name
   address_space       = var.vnet_add
-  location            = azurerm_resource_group.RG.location
-  resource_group_name = azurerm_resource_group.RG.name
+  location            = var.rg_location
+  resource_group_name = data.azurerm_resource_group.RG.name 
 }
