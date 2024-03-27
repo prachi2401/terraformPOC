ff5ec607277c74370a55be0c543f6e72aadd85ca
prachi2401 <108337953+prachi2401@users.noreply.github.com>
2024-03-27 10:41:49 +0000

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -16,6 +16,8 @@ terraform {
 }
 
 provider "azurerm" {
+  tenant_id                  = "e4e34038-ea1f-4882-b6e8-ccd776459ca0"
+  subscription_id            = "e998d3e7-b93b-4cf2-8087-c1fbe787c337"
   skip_provider_registration = true
   use_oidc                   = true
   features {}
