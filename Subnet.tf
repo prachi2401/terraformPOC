3b253e97d81970a27918e332464a5055f71bfbf0
prachi2401 <108337953+prachi2401@users.noreply.github.com>
2024-03-27 09:56:02 +0000

    Update Subnet.tf

--- Subnet.tf
+++ Subnet.tf
@@ -1,6 +1,6 @@
 resource "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
-  resource_group_name  = azurerm_resource_group.RG.name
+  resource_group_name  = data.azurerm_resource_group.RG.name
   virtual_network_name = azurerm_virtual_network.Vnet.name
   address_prefixes = var.subnet_add
 }
