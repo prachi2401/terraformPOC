04953d5da29c937e6c2a963b9b5ea1ffb3ba31db
prachi2401 <108337953+prachi2401@users.noreply.github.com>
2024-03-27 11:05:23 +0000

    Update RG_Var.tf

--- RG_Var.tf
+++ RG_Var.tf
@@ -6,18 +6,4 @@ variable "rg_location"{
   type = string
   default = "Central India"
 }
-variable "ARM_CLIENT_ID" {
-  description = "Azure Service Principal Client ID"
-}
 
-variable "ARM_CLIENT_SECRET" {
-  description = "Azure Service Principal Client Secret"
-}
-
-variable "ARM_SUBSCRIPTION_ID" {
-  description = "Azure Subscription ID"
-}
-
-variable "ARM_TENANT_ID" {
-  description = "Azure Tenant ID"
-}
