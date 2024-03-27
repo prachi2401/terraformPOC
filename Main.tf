254a93a0816e33293d871d53ba1418cbd5c4cae5
prachi2401 <108337953+prachi2401@users.noreply.github.com>
2024-03-27 07:01:48 +0000

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,5 +1,19 @@
-provider "azurerm"{
-  features {}
- 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+  backend "azurerm" {
+      resource_group_name  = "Gaurav_RG"
+      storage_account_name = "storage132"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
+
 }
 
+provider "azurerm" {
+  features {}
+}
